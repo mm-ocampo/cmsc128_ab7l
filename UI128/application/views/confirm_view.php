4188c9019b55b5679088a64a1d02de1b47012661
isko-algorithm <gjpgagno@gmail.com>
2014-03-11 16:27:22 +0800

    System as of 3/11/2014 4:00 PM, pre-testing release

--- UI128/application/views/confirm_view.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-        <title>Success</title>
-    </head>
-    <body>
-        <h1>Success!</h1>
-        <a href= "<?php echo base_url(); ?>/index.php/site/search">Back to Search</a>
-    </body>
-</html>
