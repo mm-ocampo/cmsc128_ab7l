41c164f7061fe127a14d6cf7403c1b53f90df080
cryzereye <cryzereye@gmail.com>
2014-03-20 00:15:57 +0800

    Update messages.php

--- ICSelib/application/views/messages.php
+++ ICSelib/application/views/messages.php
@@ -1,39 +1,32 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $i = 0;
-$temp = "";
-
+echo "<div id='message_results'>";
 foreach($result as $row){
-	echo "<div id='messages_result'>";
-	foreach ($row as $data) {
-		if($i==0){
-			$temp = $data;
-			echo "<div id='messages_id'>".$temp."</div>";
-		}
-		else if($i == 1){
-			echo "<div class='col-md-3'>".$data."</div>";
-		}
-		else if($i == 2){
-			echo "<div id='messages_message'>".$data."</div>";
-		}
-		else{
-			//echo "<div class='col-md-5'>".$data."</div>";
-		}
-
-		$i++;
-	}
-	echo "<div class='col-md-1'><a name='link' id='link' href='".base_url()."index.php/query/deletemessage?id={$temp}&confirm=' onclick='confirm_delete()'><button name='".$temp."'><span class='glyphicon glyphicon-trash'></span></button></a></div>";
-	$i = 0;
-	echo "</div>";
+		echo "<div class='results'>";
+            echo "<div class='result_information'>";
+				$id = $row->id;
+				$email = $row->email;
+				$header = $row->header;
+				$message = $row->message;
+				echo "<ul>";
+				echo "<li class='result_header'><i class='fa fa-book fa-lg space'></i>".$header."<input type='button' class='message_delete' onclick='confirm_delete({$i})' id='link{$i}' name='".$id."' value='Delete' /></li>";
+                echo "<div class='result_details'>";
+                		echo "<li>From: ".$email."</li>";
+                		echo "<li>Message: ".$message."</li>";
+				echo "</div>";
+			echo "</div>";
+		echo "</div>";
+	$i += 1;
 }
+echo "</div>";
 
 ?>
 <script type='text/javascript' language='javascript'>
 
-    function confirm_delete(){
-    var temp = confirm("Do you really want to delete this message?");
-    
-    /* changes value of href in link to pass variable to isset function in delete function in site.php */
-    document.getElementById("link").href = document.getElementById("link").href + temp;
-}
+    function confirm_delete(num){
+        var temp = confirm("Do you really want to delete this material?");
+
+        location.replace("<?php echo base_url();?>index.php/query/deletemessage?id=" + document.getElementById('link' + num).name + "&confirm=" + temp);
+    }
 </script>
